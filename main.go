1182ec04a2fd859b7913970b197a5807db1e9a2c
u2d-man <kazu.iosDev@gmail.com>
2026-03-21 11:32:51 +0900

    Basic Process Monitoring

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
+)
 
 func main() {
-	fmt.Println("hello")
+	var dir string
+	for i, v := range os.Args {
+		if i == 1 {
+			dir = v
+		}
+	}
+
+	absDir, err := filepath.Abs(dir)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	runPath := filepath.Join(absDir, "run.bat")
+	fmt.Println(runPath)
+
+	for {
+		cmd := exec.Command(runPath)
+		cmd.Dir = dir
+
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err = cmd.Start()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		err = cmd.Wait()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("end child process")
+
+		time.Sleep(1 * time.Second)
+
+	}
+
 }
